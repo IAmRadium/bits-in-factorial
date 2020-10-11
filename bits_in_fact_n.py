e1c8cdf252403a111aa6a194c3d9605ffe07cc05
Md Rasid Ali <45933730+IAmRadium@users.noreply.github.com>
2020-10-11 09:26:04 +0530

    Update bits_in_fact_n.py

--- bits_in_fact_n.py
+++ bits_in_fact_n.py
@@ -1,8 +1,7 @@
 import math
 a=1
-n=raw_input("enter n: ")
+n=int(raw_input("enter n: "))
 for i in range(n):
 	a=a*(i+1)
-	print i+1,
+	#print i+1,
 print "Number of bits is: "+str(math.log(a,2))
-
